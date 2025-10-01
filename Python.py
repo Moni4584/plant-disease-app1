5468f945271ebcc8c8d36527a9d323bf8c2362e9
Moni4584 <coutinhomonica65@gmail.com>
2025-10-01 15:00:13 +0530

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,7 @@
+# To install TensorFlow, run the following command in your terminal or command prompt:
+# pip install tensorflow
+
+# Your Python code goes below
+# For example:
+import tensorflow as tf
+# ...your code...
